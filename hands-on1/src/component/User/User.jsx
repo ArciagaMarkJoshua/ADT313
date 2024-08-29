801aaccb20bccb6f5a9f438116a2f8a93ab07b82
Mark Joshua Arciaga <arciagamarkjoshua@gmail.com>
2024-08-29 21:34:18 +0800

    myreactjs app

---
+++ hands-on1/src/component/User/User.jsx
@@ -0,0 +1,9 @@
+function User() {
+    return (
+        <>
+            <h1>Mark Joshua P. Arciaga</h1>
+            <h2>BSIT3B</h2>
+            <p>Im quite chill</p></>
+    )
+}
+export default User;

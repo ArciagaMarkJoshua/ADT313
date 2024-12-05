55a969ca8bd68b993ad7a34c7b1612290023ca1a
Arciaga, Mark Joshua P. <147915584+ArciagaMarkJoshua@users.noreply.github.com>
2024-12-05 21:45:28 +0800

    Displaying Cast, Photos, And Videos in the Form.jsx
    edit lang para maiba commit message di ko po alam pano eh nang hindi nagagalaw yung code changes history hehe

--- admin-semisproject-mark-bsit3b/src/utils/hooks/useDebounce.jsx
+++ admin-semisproject-mark-bsit3b/src/utils/hooks/useDebounce.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 // ----------------------------------------------------------------------
 
-export function useDebounce(value, delay = 500) {
+export function useDebounce(value, delay = 600) {
   const [debouncedValue, setDebouncedValue] = useState(value);
 
   useEffect(() => {
